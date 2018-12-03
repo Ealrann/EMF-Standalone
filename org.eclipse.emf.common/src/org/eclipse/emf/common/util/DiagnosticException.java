852af51d19492e28eac36ad7d0ff1e80a585196d
Aurélien Mora <ealrann@gmail.com>
2018-12-03 13:58:25 +0800

    Copy EMF projects from repo

---
+++ org.eclipse.emf.common/src/org/eclipse/emf/common/util/DiagnosticException.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2005-2006 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *   IBM - Initial API and implementation
+ */
+package org.eclipse.emf.common.util;
+
+import org.eclipse.core.runtime.CoreException;
+
+/**
+ * A checked exception representing a diagnosed failure.
+ * <p>
+ * Diagnostic exceptions contain a diagnostic describing the cause of the exception.
+ * </p>
+ * @see Diagnostic
+ */
+public class DiagnosticException extends Exception 
+{
+  private static final long serialVersionUID = 1L;
+
+  private Diagnostic diagnostic;
+
+  public DiagnosticException(Diagnostic diagnostic) 
+  {
+    super(diagnostic.getMessage(), diagnostic.getException());
+    this.diagnostic = diagnostic;
+  }
+  
+  public final Diagnostic getDiagnostic() 
+  {
+    return diagnostic;
+  }
+  
+  public static CoreException toCoreException(DiagnosticException exception)
+  {
+    return new CoreException(BasicDiagnostic.toIStatus(exception));
+  }
+}
