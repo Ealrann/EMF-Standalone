d554c50216d27aaac62bea7c8ca3e8a2c672d2ef
Aurélien Mora <ealrann@gmail.com>
2019-06-26 10:39:40 -0600

    Import sources (R2_32_0)

---
+++ org.eclipse.emf.ecore.xmi/src/org/eclipse/emf/ecore/xmi/impl/XMILoadImpl.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2002-2006 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *   IBM - Initial API and implementation
+ */
+package org.eclipse.emf.ecore.xmi.impl;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+import org.eclipse.emf.ecore.xmi.XMLHelper;
+
+/**
+ * This class creates an XMIHandler rather than an
+ * XMLHandler so an XMI file can be loaded.
+ */
+public class XMILoadImpl extends XMLLoadImpl {
+
+  /**
+   * Constructor for XMILoad.
+   */
+  public XMILoadImpl(XMLHelper helper) {
+    super(helper);
+  }
+
+  @Override
+  protected DefaultHandler makeDefaultHandler()
+  {
+    return new SAXXMIHandler(resource, helper, options);
+  }
+
+}
